cc41b8ae962e9f55148303d0c5b338d6bf67cb52
Sanjeeb Nath <59302663+SanjeebNath382@users.noreply.github.com>
2020-05-04 12:35:04 +0530

    new change
    new change

---
+++ home2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        
+        
+
+    </head>
+    <body>
+        <div class="row">
+                <img src="./assests/logo.png" alt="logo" style="top:63px;left:50px;width:253px;height:77px">
+        </div>
+    </body>
+</html>
